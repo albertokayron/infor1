05efae0d270df899565a4a55e33e7a7e41c07a6d
DevCarlosSilva <carlos.silva1704@aluno.ce.gov.br>
2025-08-18 12:01:17 -0300

    lista 2

---
+++ lista 2.py
@@ -0,0 +1,33 @@
+
+#     nota=int(input("Digite a nota: "))
+#     if nota >= 7:
+#         print("Aprovado")
+#     else:
+#         print("Reprovado")
+
+# soma=0
+# palavra = (input("Fale uma palavra: "))
+# vogais = ['a' , 'e' , 'i' , 'o' , 'u']
+# for i in palavra:
+#     if i in vogais:
+#         soma += 1 
+# print(soma)
+    
+# numeroint= int(input("Me fale um numero inteiro: "))
+# if numeroint % 2 == 0:
+#     print("Esse número é par")
+# else:
+#     print("Esse numero é impar")
+
+# for i in range(1,6):
+#     num=int(input("escreva um número: "))
+#     if i == 1:
+#         maior = menor = num   
+#     elif num > maior:
+#         maior= num
+#     elif  num < menor:  
+#         menor = num
+
+# print(f'Maior número: {maior}')
+# print(f'Menor número: {menor}')
+
